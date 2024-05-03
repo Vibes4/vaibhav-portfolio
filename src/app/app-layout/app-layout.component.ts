bbfaf08cfdd133044a3226087ca2e2bfcb914975
vaibhav <vaibhavkulkarni1540@gamail.com>
2024-05-03 12:41:38 +0530

    component

--- src/app/app-layout/app-layout.component.ts
+++ src/app/app-layout/app-layout.component.ts
@@ -1,11 +1,25 @@
 import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-
+import {
+  NavigationStart,
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
+import { Subscription, filter } from 'rxjs';
+import { HomeComponent } from '../home/home.component';
+import { AboutComponent } from '../about/about.component';
 @Component({
   selector: 'vk-app-layout',
   standalone: true,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    HomeComponent,
+    AboutComponent,
+  ],
   templateUrl: './app-layout.component.html',
   styleUrl: './app-layout.component.scss',
 })
@@ -13,10 +27,20 @@ export class AppLayoutComponent {
   menuItems: Array<{ display: string; value: string }> = [
     { display: 'Home', value: 'home' },
     { display: 'About', value: 'about' },
+    { display: 'What I Know', value: 'what-i-know' },
     { display: 'Contact-me', value: 'contact-me' },
   ];
-
+  private routerEventSubscriber!: Subscription;
+  isLogoHidden: Boolean = false;
+  constructor(private router: Router) {
+    this.routerEventSubscriber = this.router.events
+      .pipe(filter((event) => event instanceof NavigationStart))
+      .subscribe((x: any) => (this.isLogoHidden = x.url !== '/home'));
+  }
   ngOnInIt() {}
 
+  ngDestroy() {
+    this.routerEventSubscriber?.unsubscribe();
+  }
   menuClick(item: string) {}
 }
